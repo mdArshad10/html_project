05ddcca592ac2cd4c8cdf31a5ca0bb0c85adb24b
mdArshad10 <mdarshd246@gmail.com>
2023-09-01 15:13:53 +0530

    adding the mouse moving effect and some gsap properities

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("hi this is console of javascript");
+
+let crs = document.querySelector("#mouseCursor");
+
+document.addEventListener("mousemove",(detl)=>{
+    crs.style.top= detl.y+"px";
+    crs.style.left = detl.x+"px";
+})
+
+gsap.to("nav", {
+  backgroundColor: `#000`,
+  height: "120px",
+
+  scrollTrigger: {
+    trigger: "nav",
+    scroller: "body",
+    markers: true,
+    start:"75% 5%",
+    end:"20% 2%",
+
+    scrub: 2,
+  },
+});
+
+
+gsap.to("#main", {
+  backgroundColor: `#rgba(0, 0, 0, 0.243)`,
+
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers: true,
+    start:"top 10px"
+  },
+});
