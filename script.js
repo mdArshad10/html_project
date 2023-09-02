2af3b09231c15e5ece7d043ba963bf3ca57be433
mdArshad10 <mdarshd246@gmail.com>
2023-09-01 23:11:35 -0400

    createing the marquee tags

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ console.log("hi this is console of javascript");
 
 let crs = document.querySelector("#mouseCursor");
 
-document.addEventListener("mousemove",(detl)=>{
-    crs.style.top= detl.y+"px";
-    crs.style.left = detl.x+"px";
-})
+document.addEventListener("mousemove", (detl) => {
+  crs.style.top = detl.y + "px";
+  crs.style.left = detl.x + "px";
+});
 
 gsap.to("nav", {
   backgroundColor: `#000`,
@@ -14,22 +14,22 @@ gsap.to("nav", {
   scrollTrigger: {
     trigger: "nav",
     scroller: "body",
-    markers: true,
-    start:"75% 5%",
-    end:"20% 2%",
-
+    // markers: true,
+    start: "top -10%",
+    end: "top -11%",
     scrub: 2,
   },
 });
 
-
 gsap.to("#main", {
-  backgroundColor: `#rgba(0, 0, 0, 0.243)`,
+  backgroundColor: `#000`,
 
   scrollTrigger: {
     trigger: "#main",
     scroller: "body",
-    // markers: true,
-    start:"top 10px"
+    markers: true,
+    start: "top -50vh",
+    end: "top -80vh",
+    scrub: 2,
   },
 });
